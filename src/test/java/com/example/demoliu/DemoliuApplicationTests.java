7d3b148387309ed6d56ef4289e210d6372e735fe
964180688 <964180688@qq.com>
2020-06-05 16:58:16 +0800

    Initial commit

---
+++ src/test/java/com/example/demoliu/DemoliuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoliu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoliuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
